9a9fc4bbd02228c3c897aebf9cd20a3d68f1dc51
Vasiliy Tanzykov <vasiliytanzykov@gmail.com>
2024-03-06 20:15:00 +0500

    asdfasdf

--- fin_manager/users/urls.py
+++ fin_manager/users/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('analytics/', views.analytics, name='analytics'),
     path('category/', views.category, name='category'),
     path('journal/', views.journal, name='journal'),
-    path('', views.journal, name='journal'),
-    path('create/', views.create, name='create'),
+    path('number/<int:n>/<int:m>', views.number, name='number'),
 ]
--- fin_manager/users/views.py
+++ fin_manager/users/views.py
@@ -6,21 +6,23 @@ from django.contrib.auth.models import User
 
 
 def analytics(request):
+    data = request.session.get("key", 0)
+    print(data)
     return render(request, 'analytics.html')
 
 
 def journal(request):
+    data = request.session.get("key", 0) + 1
+    request.session['key'] = data
     return render(request, 'journal.html')
 
 
 def category(request):
+    request.session['key'] = 0
     return render(request, 'category.html')
 
 
-def create(request):
-    user = User.objects.create_user('noadmin')
-    user.save()
-    return HttpResponse("""
-    <p>Пользователь добавлен</p>
-    <a href="/admin">Перейти в админку</a>
+def number(request, n=0, m=0):
+    return HttpResponse(f"""
+    <h1>{n} + {m} = {n + m}</h1>
     """)
