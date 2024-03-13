c0019509836eae82ce9799dedb0a2d574dabd1e4
Vasiliy Tanzykov <vasiliytanzykov@gmail.com>
2024-03-13 19:53:48 +0500

    Классная работа

--- fin_manager/users/forms.py
+++ fin_manager/users/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
+    name = forms.CharField(label='Имя', required=True)
+    comment = forms.CharField(label='Комментарий', required=False)
+    age = forms.IntegerField(label='Возраст', initial=18)
