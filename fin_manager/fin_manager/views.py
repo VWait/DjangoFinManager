dd75d53e42e4588f76d41ce8d9031c9d8d9d11a4
Vasiliy Tanzykov <vasiliytanzykov@gmail.com>
2024-03-25 19:47:27 +0500

    Классная работа

--- fin_manager/fin_manager/views.py
+++ fin_manager/fin_manager/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import redirect, render
 def index(request):
     if request.user.is_authenticated:
         return redirect('/finance/wallet/')
-    return redirect('')
+    return redirect('home/')
 
 
 def home(request):
-    render(request, 'no_auth')
+    return render(request, 'no_auth.html')
