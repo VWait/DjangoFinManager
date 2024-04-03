1b61c7b30ad46e9b911a78a39dd9903af0a24062
Vasiliy Tanzykov <vasiliytanzykov@gmail.com>
2024-04-03 20:32:04 +0500

    Начало работы с finance

--- fin_manager/finance/models.py
+++ fin_manager/finance/models.py
@@ -3,40 +3,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Wallet(models.Model):
+class Buffer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-    value = models.IntegerField(default=0)
+    value = models.FloatField(default=0)
+    expected_value = models.FloatField(default=0)
 
     def __str__(self):
-        return f'{self.name}: {self.value} руб'
+        return f'Счёт - {self.value} руб'
+
+
+class CategoryType(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f'Категория "{self.name}"'
 
 
 class Category(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.name
+    buffer = models.ForeignKey(Buffer, on_delete=models.CASCADE)
+    category_type = models.ForeignKey(CategoryType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32)
+    date_start = models.DateField()
+    date_end = models.DateField(null=True)
+    period = models.DurationField()
+    expected_value = models.FloatField(null=True)
 
 
 class Note(models.Model):
-    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    value = models.IntegerField(null=False)
-    date_note = models.DateField(null=True)
-
-    def __str__(self):
-        return f'{self.category}: {self.value} руб'
-
-
-class Plan(models.Model):
-    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    value = models.IntegerField(default=0)
-    type = models.CharField(max_length=30)
-    date_start = models.DateField()
-    date_end = models.DateField()
-
-    def __str__(self):
-        return f'{self.type}: {self.value}'
+    date = models.DateField(null=True)
+    value = models.FloatField()
